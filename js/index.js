1eebc4a4f017e8b21b9e16533dfd1b27f0519159
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-01-25 19:41:04 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ button.addEventListener('click', () => {
 
 function checkPassword() {
     var password = document.getElementById("password").value;
-    if (password == "1") {
+    if (password == "01813") {
         document.getElementById("login").style.display = "none";
     }
     else
