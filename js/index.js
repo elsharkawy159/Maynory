4af82e7d6fd0a57941e6af0dd16f8fb405ef7c3c
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-01-31 23:32:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,6 +29,42 @@ const quotes = [
 'I have died everyday waiting for you darling, don’t be afraid I have loved you for a thousand years."',
 'If I had a flower for every time I thought of you… I could walk through my garden forever."',
 'If I know how to love, it\'s because of you.❤️"',
+'I like you very much, just as you are❤️"',
+'\'I love you\' begins by I, but it ends up by you."',
+'Your love shines in my heart as the sun that shines upon the earth."',
+'I am so in love with you that there isn\'t anything else."',
+'A hundred hearts would be too few to carry all my love for you."',
+'Being with you and not being with you is the only way I have to measure time."',
+'I find the most beautiful moments of life are\'t just with you but because of you"',
+'I love you more than I can express, or can ever hope to express."',
+'I never want to stop making memories with you."',
+'Every day I’m convinced that I can’t possibly love you more… and every day I’m proven wrong."',
+'Your words are my food, your breath is my water. You are everything to me."',
+'Till my last day, I’ll be loving you."',
+'I need you like a heart needs a beat."',
+'I am in you and you in me, mutual in divine love."',
+'If you live to be a hundred, I want to live to be a hundred minus one day so I never have to live without you."',
+'The day my life changed forever ... the day I first saw you.❤️"',
+'I love you, not only for what you are, but for what I am when I am with you."',
+'When I saw you I fell in love, and you smiled because you knew."',
+'If you remember me, then I don\'t care if everyone else forgets."',
+'I wish you to know that you have been the last dream of my soul."',
+'My night has become a sunny dawn because of you."',
+'I want you today, tomorrow, next week and for the rest of my life."',
+'I love you and that’s the beginning and end of everything."',
+'I\'ve never had a moment\'s doubt. I love you. I believe in you completely. You are my dearest one. My reason for life."',
+'I would rather spend one lifetime with you, than face all the ages of this world alone."',
+'It’s always better when we\'re together."',
+'You really make me want to be a better man."',
+'I love you. You… you complete me."',
+'My soul and your soul are forever tangled."',
+'Each day I love you more, today more than yesterday and less than tomorrow."',
+'You are the one whom my soul loves."',
+'You are my lover, my human diary and my other half. You mean the world to me and I love you."',
+'You\'re a beautiful girl, you deserve a beautiful life. Nothing less."',
+'When you look at me, when you think of me, I am in paradise."',
+'I wish I could turn back the clock. I\'d find you sooner and love you longer."',
+'It was love at first sight, at ever and ever sight."',
 // In love with..
 'I am In love with your eyes."', 
 'I am In love with your smile.❤️"', 
@@ -102,6 +138,7 @@ let disableTime = 10 * 60 * 60;
 button.addEventListener('click', () => {
 button.disabled = true;
 localStorage.setItem('button', button);
+
 setTimeout(() => {
     button.disabled = false;
 }, disableTime);
