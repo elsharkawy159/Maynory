352568660e0d6870d9b317cd4cc1f3e6d681c564
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-01-31 22:43:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,73 +1,73 @@
 
-// const quotes = [
-//     'Love You My sweety Strawberry"',
-// 'you are the light that guides me through life\'s darkest moments."',
-// 'your love is like a beacon of hope in my life."',
-// 'you are the source of my strength and courage."',
-// 'your love is like a warm embrace that never fades away."',
-// 'you are the sunshine in my life that brings me joy and happiness."',
-// 'your love is like a river that never runs dry."',
-// 'you are the one who fills my heart with unconditional love and acceptance."',
-// 'your love is like a beautiful melody that never stops playing in my heart."'
+const quotes = [
+    'Love You My sweety Strawberry"',
+'you are the light that guides me through life\'s darkest moments."',
+'your love is like a beacon of hope in my life."',
+'you are the source of my strength and courage."',
+'your love is like a warm embrace that never fades away."',
+'you are the sunshine in my life that brings me joy and happiness."',
+'your love is like a river that never runs dry."',
+'you are the one who fills my heart with unconditional love and acceptance."',
+'your love is like a beautiful melody that never stops playing in my heart."'
 
 
-// ];
+];
 
-// const userName = [
-//     '"Maynory, ',
-//     '"My Soulmate, ',
-//     '"Sweety, ',
-//     '"Strawberry🍓, ',
-//     '"Twenkez, ',
-//     '"Baskota, ',
-//     '"My Moon, ',
-//     '"My Twins, ',
-//     '"Koky, ',
-//     '"Frawla🍓, ',
-//     '"My Other Half, ',
-//     '"My Queen, ',
-//     '"Sweetheart, ',
-//     '"Honey, ',
-//     '"Sugar, ',
-//     '"Flower, ',
-//     '"Sunshine, ',
-//     '"Kitty, ',
-//     '"Darling, ',
-//     '"My Lady, ',
-//     '"Sweetie, ',
-//     '"Cookie, ',
-//     '"Baby, ',
-//     '"Caramela, ',
-//     '"Lollipop, ',
-//     '"Rouhi, ',
-//     '"Habibty, ',
-//     '"3omry, ',
-// ]
+const userName = [
+    '"Maynory, ',
+    '"My Soulmate, ',
+    '"Sweety, ',
+    '"Strawberry🍓, ',
+    '"Twenkez, ',
+    '"Baskota, ',
+    '"My Moon, ',
+    '"My Twins, ',
+    '"Koky, ',
+    '"Frawla🍓, ',
+    '"My Other Half, ',
+    '"My Queen, ',
+    '"Sweetheart, ',
+    '"Honey, ',
+    '"Sugar, ',
+    '"Flower, ',
+    '"Sunshine, ',
+    '"Kitty, ',
+    '"Darling, ',
+    '"My Lady, ',
+    '"Sweetie, ',
+    '"Cookie, ',
+    '"Baby, ',
+    '"Caramela, ',
+    '"Lollipop, ',
+    '"Rouhi, ',
+    '"Habibty, ',
+    '"3omry, ',
+]
 
-// var count = 0;
+var count = 0;
 
-// function printQuote() {
-//     let randomNumber = Math.floor(Math.random() * quotes.length);
-//     let randomName = Math.floor(Math.random() * userName.length);
-//     count++;
-//     console.log(quotes[randomNumber]);   // Prints quote to console for testing purposes
+function printQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    let randomName = Math.floor(Math.random() * userName.length);
+    count++;
+    console.log(quotes[randomNumber]);   // Prints quote to console for testing purposes
 
-//     document.getElementById("quote").innerHTML = "❤️" + userName[randomName] + quotes[randomNumber] + "❤️";   // Prints quote to HTML element with id="quote"
+    document.getElementById("quote").innerHTML = "❤️" + userName[randomName] + quotes[randomNumber] + "❤️";   // Prints quote to HTML element with id="quote"
 
-//     document.getElementById("count").innerHTML = count;
-//     console.log(count)
-// }
+    document.getElementById("count").innerHTML = count;
+    console.log(count)
+}
 
 
 
-// let button = document.getElementById('button');
-// let disableTime = 10 * 60 * 13;
-// button.addEventListener('click', () => {
-//   button.disabled = true;
-//   setTimeout(() => {
-//     button.disabled = false;
-//   }, disableTime);
-// });
+let button = document.getElementById('button');
+let disableTime = 10 * 60 * 13;
+button.addEventListener('click', () => {
+  button.disabled = true;
+  setTimeout(() => {
+    button.disabled = false;
+  }, disableTime);
+});
 
 
 
