c20ff407efbaad44ee0006a3f7502ad1bbffd33b
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-02-24 03:20:09 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-console.log("Pushed");
+console.log("Pushed1");
 const quotes = [
 'Love You so much My sweety Strawberry.❤️"',
 'You are the light that guides me through life\'s darkest moments.❤️"',
@@ -212,27 +212,27 @@ function copyElement() {
 
     // disable right click >>>>>>
 
-//     document.addEventListener('contextmenu', event => event.preventDefault());
+    document.addEventListener('contextmenu', event => event.preventDefault());
 
-//     document.onkeydown = function (e) {
+    document.onkeydown = function (e) {
 
-//         // disable F12 key
-//         if(e.keyCode == 123) {
-//             return false;
-//         }
+        // disable F12 key
+        if(e.keyCode == 123) {
+            return false;
+        }
 
-//         // disable I key
-//         if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-//             return false;
-//         }
+        // disable I key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+            return false;
+        }
 
-//         // disable J key
-//         if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-//             return false;
-//         }
+        // disable J key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            return false;
+        }
 
-//         // disable U key
-//         if(e.ctrlKey && e.keyCode == 85) {
-//             return false;
-//         }
-//     }
+        // disable U key
+        if(e.ctrlKey && e.keyCode == 85) {
+            return false;
+        }
+    }
